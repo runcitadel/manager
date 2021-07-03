c38b9c39a4eb7e5c53ac49b19c43f2a7f11f3d3c
Aaron Dewes <aaron.dewes@web.de>
2021-07-03 13:05:24 +0100

    Remove JS code from the repo

--- middlewares/camelCaseRequest.js
+++
@@ -1,7 +0,0 @@
-import camelize from 'camelize-ts';
-export function camelCaseRequest(request, res, next) {
-    if (request && request.body) {
-        request.body = camelize(request.body);
-    }
-    next();
-}
--- routes/ping.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-// @ts-ignore
-import * as pjson from '../package.json';
-const router = Router();
-router.get('/', (request, res) => {
-    res.json({ version: 'umbrel-manager-' + pjson.version });
-});
-module.exports = router;

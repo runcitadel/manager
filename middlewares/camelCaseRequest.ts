6cc6f0953363b6dfa8244d6d41c5b2597dd72337
Aaron Dewes <aaron.dewes@web.de>
2021-07-04 08:44:27 +0100

    Maybe this works better

--- middlewares/camelCaseRequest.ts
+++ middlewares/camelCaseRequest.ts
@@ -1,5 +1,24 @@
-import camelize from 'camelize-ts';
 import { Request, Response, NextFunction } from 'express';
+
+const toCamel = (s: string) => {
+    return s.replace(/([-_][a-z])/ig, ($1) => {
+      return $1.toUpperCase()
+        .replace('-', '')
+        .replace('_', '');
+    });
+  };
+
+ function camelize (object: Object) {
+    if (typeof object === 'string') return toCamel(object);
+    if(typeof object !== "object") return object;
+    return Object
+      .entries(object)
+      .reduce((carry, [key, value]) => {
+        // @ts-expect-error
+        carry[toCamel(key)] = value
+        return carry
+      }, {})
+  }
 
 export function camelCaseRequest(request: Request, res: Response, next: NextFunction) {
     if (request && request.body) {

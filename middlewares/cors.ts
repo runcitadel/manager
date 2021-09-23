936182889658f7dbc5858ebc6e3c9a5be00b1dc9
Aaron Dewes <aaron.dewes@web.de>
2021-09-23 19:54:22 +0100

    Add save mode, some refactoring

--- middlewares/cors.ts
+++ middlewares/cors.ts
@@ -1,4 +1,5 @@
 import * as process from 'node:process';
+import {URL} from 'node:url';
 import {Context} from 'koa';
 
 export const corsOptions = {
@@ -6,16 +7,31 @@ export const corsOptions = {
     const origin =
       ctx.headers.origin ?? ctx.request.headers.origin ?? ctx.origin;
     const allowList = [
-      'http://localhost:3000',
-      'http://localhost:8080',
       'http://localhost',
       'http://umbrel.local',
       ...process.env.DEVICE_HOSTS!.split(','),
     ];
 
-    if (allowList.includes(origin) || !origin) {
-      return '';
+    // Allowlist, but URL.parse every element, and only get the domain
+    const allowedDomains = [];
+    let url: string;
+    for (url of allowList) {
+      const parsed = new URL(url);
+      allowedDomains.push(parsed.hostname);
     }
+
+    if (!process.env.SAFE_MODE) {
+      try {
+        const parsed = new URL(origin);
+        if (allowedDomains.includes(parsed.hostname)) {
+          return parsed.origin;
+        }
+      } catch {
+        if (!origin) return '*';
+      }
+    }
+
+    if (allowList.includes(origin)) return origin;
 
     throw new Error('Not allowed by CORS');
   },

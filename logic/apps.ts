f23a8aafae6b0ce30b65cb6f554bf10e3922cc3f
AaronDewes <aaron.dewes@protonmail.com>
2022-04-20 17:58:13 +0000

    Fixes for app.yml v3

--- logic/apps.ts
+++ logic/apps.ts
@@ -1,27 +1,47 @@
 import * as lightningService from '../services/lightning-api.js';
 import * as diskLogic from './disk.js';
 
+/** A dependency an app could have */
+export type Dependency = 'bitcoind' | 'electrum' | 'lnd' | 'c-lightning';
+
 export type App = {
+  /** The id of the app, the name as a simple string without spaces */
   id: string;
+  /** A category for the app, used for grouping apps on the dashboard */
   category: string;
+  /** The name of the app */
   name: string;
+  /** The version of the app */
   version: string;
+  /** A One line description of the app (max 50 characters) */
   tagline: string;
+  /** A longer description of the app (50 to 200 words) */
   description: string;
-  developer: string;
-  website: string;
-  dependencies: string[];
-  repo: string;
+  /** The person(s) who created the app */
+  developer?: string;
+  /** The person(s) who created the app */
+  developers?: Record<string, string>;
+  /** The dependencies of the app */
+  dependencies: Array<Dependency | Dependency[]>;
+  /** The url to the app's Git repository */
+  repo: string | Record<string, string>;
+  /** The url to the app's support website/chat */
   support: string;
+  /** The port the app's web UI uses */
   port: number;
+  /** A list of links to app promotional images, if no domain is provided, https://runcitadel.github.io/old-apps-gallery/${app.id}/ will be put in front of the path */
   gallery: string[];
+  /** The path of the app the open button should open */
   path: string;
+  /** The app's default password */
   defaultPassword: string;
+  /** Automatically added */
   hiddenService?: string;
+  /** Automatically added */
   installed?: boolean;
+  /** Automatically added */
   compatible: boolean;
 };
-
 export type AppQuery = {
   installed?: boolean;
 };
@@ -38,15 +58,27 @@ export async function get(query: AppQuery, jwt: string): Promise<App[]> {
         app.hiddenService = '';
       }
 
-      if (
-        (app.dependencies.includes('lnd') &&
-          lightningImplementation === 'c-lightning') ||
-        (app.dependencies.includes('c-lightning') &&
-          lightningImplementation === 'lnd')
-      ) {
-        app.compatible = false;
-      } else {
-        app.compatible = true;
+      for (const dependency of app.dependencies) {
+        if (typeof dependency === 'string') {
+          if (
+            (dependency === 'c-lightning' &&
+              lightningImplementation === 'lnd') ||
+            (dependency === 'lnd' && lightningImplementation === 'c-lightning')
+          ) {
+            app.compatible = false;
+            // Skip validating other dependencies
+            continue;
+          }
+        } else if (
+          (dependency.includes('c-lightning') &&
+            lightningImplementation === 'lnd') ||
+          (dependency.includes('lnd') &&
+            lightningImplementation === 'c-lightning')
+        ) {
+          app.compatible = false;
+          // Skip validating other dependencies
+          continue;
+        }
       }
     }),
   );

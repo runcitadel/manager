a73abc7ebb66598f428ba761bbf00bc8b9d35b75
AaronDewes <aaron.dewes@protonmail.com>
2022-08-30 12:54:41 +0000

    Add check for flash
    To support older Deno versions

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 import {
   Application,
   FlashServer,
+  hasFlash,
 } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import logger from "https://deno.land/x/oak_logger@1.0.0/mod.ts";
@@ -15,7 +16,9 @@ import apps from "./routes/v1/apps.ts";
 
 config();
 
-const app = new Application({ serverConstructor: FlashServer });
+const appOptions = hasFlash() ? { serverConstructor: FlashServer } : undefined;
+
+const app = new Application(appOptions);
 
 app.use(logger.logger);
 app.use(logger.responseTime);

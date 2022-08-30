86b5655d7d25ee543dc451254db692be17399e15
Aaron Dewes <aaron.dewes@protonmail.com>
2022-08-30 21:48:48 +0200

    Try to fix karen connection

--- services/karen.ts
+++ services/karen.ts
@@ -1,10 +1,8 @@
 
 import {KAREN_SOCKET} from '../utils/const.ts';
 
-let connection: Deno.UnixConn;
-
 export async function runCommand(command: string) {
-  if(!connection)
-    connection = await Deno.connect({ path: KAREN_SOCKET, transport: "unix" });
+  let connection = await Deno.connect({ path: KAREN_SOCKET, transport: "unix" });
   await connection.write(new TextEncoder().encode(command));
+  await connection.close();
 }
